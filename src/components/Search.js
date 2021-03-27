3bfa0cd89d12e60f7b73219c259d82b28ddd8958
Burak Gündüz <burakkgunduz21@gmail.com>
2021-03-27 16:37:56 +0300

    litle fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,16 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import {FaSearchDollar} from "react-icons/fa"
 import { BudgetContextFunc } from './BudgetContext'
 import "./Search.css"
 
 const Search = () => {
-    const [keyword, setKeyword] = useState("")
     const {budgetList, setBudgetList} = useContext(BudgetContextFunc);
    
-
     return (
         <form action="/" method="get">
             <label htmlFor="search-input"><span className="visually-hidden">Search Expenses</span></label>
-            <input name="s" id="search-input" onChange={(e) => setKeyword(e.target.value)} className="rounded-pill" type="text" placeholder="Search expenses..."></input>
+            <input id="search-input" className="rounded-pill" type="text" placeholder="Search expenses..."></input>
             <div className="ml-2"><FaSearchDollar type="submit" style={{color: "white"}} size="1.4em"></FaSearchDollar></div>  
-            <button type="submit">ilet</button>
         </form>
     )
 }
