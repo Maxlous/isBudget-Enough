478f19124b7270445c33c012ad025503e8d6fe06
Burak Gündüz <burakkgunduz21@gmail.com>
2021-03-28 13:19:48 +0300

    search component is functional now

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,17 +1,32 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { BudgetContextFunc } from './BudgetContext'
 import ExpenseItem from "./ExpenseItem"
+import Search from "./Search.js"
 
 const Expenses = () => {
-
+    const [keyword, setKeyword] = useState("")
     const {budgetList} = useContext(BudgetContextFunc)
+    const [searchResults, setSearchResults] = useState(budgetList)
+    
+    useEffect(() => {
+        const results = budgetList.filter((item) => item.name.toLowerCase().includes(keyword));
+        setSearchResults(results);
+    },[keyword, budgetList])
 
     return (
-        <ul className="list-group">
-            {budgetList.map(item => (
-                <ExpenseItem key={item.id} itemID={item.id} name={item.name} cost={item.cost}/>
-            ))}
-        </ul>
+        <>
+            <section className="row">
+                <div className="col-sm d-flex justify-content-center">
+                    <Search setKeyword={setKeyword} />
+                 </div>
+            </section>
+            <h3 style={{color:"white"}} className="font-weight-light text-center">Expenses</h3>
+            <ul className="list-group">
+                {searchResults.map(item => (
+                    <ExpenseItem key={item.id} itemID={item.id} name={item.name} cost={item.cost}/>
+                ))}
+            </ul>
+        </>
     )
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,14 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import {FaSearchDollar} from "react-icons/fa"
-import { BudgetContextFunc } from './BudgetContext'
 import "./Search.css"
 
-const Search = () => {
-    const {budgetList, setBudgetList} = useContext(BudgetContextFunc);
-   
+
+const Search = ({setKeyword}) => {
+    
     return (
-        <form action="/" method="get">
+        <form className="d-inline-flex mb-3" action="/" method="get">
             <label htmlFor="search-input"><span className="visually-hidden">Search Expenses</span></label>
-            <input id="search-input" className="rounded-pill" type="text" placeholder="Search expenses..."></input>
+            <input onChange={(e) => setKeyword(e.target.value)} id="search-input" className="rounded-pill" type="text" placeholder="Search expenses..."></input>
             <div className="ml-2"><FaSearchDollar type="submit" style={{color: "white"}} size="1.4em"></FaSearchDollar></div>  
         </form>
     )
