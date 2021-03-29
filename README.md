3c0420c74f01b7cedabe96839e288772e7c08b2f
Burak Gündüz <burakkgunduz21@gmail.com>
2021-03-29 18:39:25 +0300

    meta content edited, readme.md added

---
+++ README.md
@@ -0,0 +1,9 @@
+## Budget Tracker App
+
+- In order to add expenses, search them, or delete them, [GO TO LIVE WEBSITE!](https://isbudgetenough.netlify.app/)
+
+---
+
+- This is a web app created with React. The app saves the data in localStorage.
+- It benefits from React Hooks, uuid, bootstrap, and react-icons.
+- You can clone and npm start right after you installed the node_modules.
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,11 +6,11 @@ import "./Search.css"
 const Search = ({setKeyword}) => {
     
     return (
-        <form className="d-inline-flex mb-3" action="/" method="get">
+        <div className="d-inline-flex mb-3">
             <label htmlFor="search-input"><span className="visually-hidden">Search Expenses</span></label>
             <input onChange={(e) => setKeyword(e.target.value)} id="search-input" className="rounded-pill" type="text" placeholder="Search expenses..."></input>
-            <div className="ml-2"><FaSearchDollar type="submit" style={{color: "white"}} size="1.4em"></FaSearchDollar></div>  
-        </form>
+            <div className="ml-2"><FaSearchDollar style={{color: "white"}} size="1.4em"></FaSearchDollar></div>  
+        </div>
     )
 }
 
